2d857315017e2007400ec55e2477860e66f6b246
Keerthi-bot-sudo <56e3keerthananomula@gmail.com>
2025-10-31 07:08:13 -0700

    Add files via upload

---
+++ c program 1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+		int a,b,sum;    //variable declaration
+		printf("enter any two numbers\n");
+		scanf("%d%d",&a,&b);
+		sum=a+b;
+		printf("sum of above two numbers=%d",sum);
+}
